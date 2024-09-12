8c7dcad1b45d40d9a3daa679006c21dde3477885
driftdev <92285161+driftdev@users.noreply.github.com>
2024-09-12 21:13:16 +0530

    refactor(eventstore): renamed db fields to align with the db schema

--- internal/eventstore/push.sql
+++ internal/eventstore/push.sql
@@ -1,3 +1,4 @@
-insert into events (tenant_id, aggregate_type, aggregate_version, aggregate_id, aggregate_sequence, event_type, data,
-                    owner, creator, correlation_id, causation_id, position, created_at)
-values %s returning "position", created_at;
+insert into events (tenant_id, aggregate_type, aggregate_version, aggregate_id, aggregate_sequence, event_type,
+                    data, resource_owner, creator, correlation_id, causation_id, global_position, created_at)
+values
+%s returning global_position, created_at;
