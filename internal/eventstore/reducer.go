824bba1fbc24c87f41ecfe3c41efca15548e8cc4
driftdev <92285161+driftdev@users.noreply.github.com>
2024-09-19 21:34:02 +0530

    feat(eventstore): implemented the event reduce function to build state from event

--- internal/eventstore/reducer.go
+++ internal/eventstore/reducer.go
@@ -1,6 +1,11 @@
 package eventstore
 
-import "context"
+import (
+	"context"
+	"github.com/driftbase/auth/internal/database"
+	"github.com/jackc/pgx/v5"
+	"strconv"
+)
 
 type Reducer interface {
 	Query() *SearchQueryBuilder
@@ -8,6 +13,52 @@ type Reducer interface {
 	Reduce() error
 }
 
-func (es *EventStore) Reducer(ctx context.Context, searchQueryBuilder *SearchQueryBuilder, reducer Reducer) error {
-	return nil
+func (es *EventStore) Reduce(ctx context.Context, reducer Reducer) error {
+	stmt, args, err := reducer.Query().ToSql()
+	if err != nil {
+		return err
+	}
+
+	rows, err := es.db.Query(ctx, stmt, args)
+	if err != nil {
+		return err
+	}
+
+	eventRows, err := pgx.CollectRows(rows, pgx.RowToAddrOfStructByName[database.Event])
+	if err != nil {
+		return err
+	}
+
+	for _, eventRow := range eventRows {
+		unmappedEvent := &event{
+			aggregate: NewAggregate(
+				TenantId(eventRow.TenantId),
+				AggregateType(eventRow.AggregateType),
+				AggregateVersion("v"+strconv.Itoa(eventRow.AggregateVersion)),
+				AggregateId(eventRow.AggregateId),
+				eventRow.ResourceOwner.String,
+				uint64(eventRow.AggregateSequence),
+			),
+			eventType: EventType(eventRow.EventType),
+			payload:   eventRow.Payload,
+			createdAt: eventRow.CreatedAt,
+		}
+		if eventRow.CorrelationId.Valid {
+			correlationId := eventRow.CorrelationId.String
+			unmappedEvent.correlationId = &correlationId
+		}
+		if eventRow.CausationId.Valid {
+			causationId := eventRow.CausationId.String
+			unmappedEvent.causationId = &causationId
+		}
+
+		mappedEvent, err := es.mapEvent(unmappedEvent)
+		if err != nil {
+			return err
+		}
+
+		reducer.AppendEvents(mappedEvent)
+	}
+
+	return reducer.Reduce()
 }
