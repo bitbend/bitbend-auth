547f6c304266e70c742e2eab11e1a15dcfc63d17
driftdev <92285161+driftdev@users.noreply.github.com>
2024-09-20 21:41:54 +0530

    feat(eventstore): implemented get tenantIds and get current global position methods

--- internal/eventstore/eventstore.go
+++ internal/eventstore/eventstore.go
@@ -7,6 +7,9 @@ import (
 	"github.com/driftbase/auth/internal/sverror"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/shopspring/decimal"
+	"github.com/stephenafamo/bob/dialect/psql"
+	"github.com/stephenafamo/bob/dialect/psql/sm"
 	"sync"
 	"time"
 )
@@ -14,7 +17,7 @@ import (
 type EventStore struct {
 	PushTimeout     time.Duration
 	maxRetries      int
-	tenants         []string
+	tenantIds       []TenantId
 	lastTenantQuery time.Time
 	tenantMutex     sync.Mutex
 	db              *pgxpool.Pool
@@ -50,4 +53,58 @@ func (es *EventStore) withTxn(ctx context.Context, txOptions pgx.TxOptions, fn f
 	}
 
 	return nil
+}
+
+func (es *EventStore) GetTenantIds(ctx context.Context, fromDb bool) ([]TenantId, error) {
+	if !fromDb {
+		return es.tenantIds, nil
+	}
+
+	stmt, args, err := psql.Select(
+		sm.From("events"),
+		sm.Columns("tenant_id"),
+		sm.Distinct("tenant_id"),
+	).Build()
+	if err != nil {
+		return nil, sverror.NewInternalError("error.failed.to.build.query", err)
+	}
+
+	rows, err := es.db.Query(ctx, stmt, args...)
+	if err != nil {
+		return nil, sverror.NewInternalError("error.failed.to.query", err)
+	}
+
+	tenantIdRows, err := pgx.CollectRows(rows, pgx.RowTo[string])
+	if err != nil {
+		return nil, sverror.NewInternalError("error.failed.to.collect.rows", err)
+	}
+
+	tenantIds := make([]TenantId, 0)
+	for _, tenantIdRow := range tenantIdRows {
+		tenantIds = append(tenantIds, TenantId(tenantIdRow))
+	}
+
+	return tenantIds, nil
+}
+
+func (es *EventStore) GetCurrentGlobalPosition(ctx context.Context) (*decimal.Decimal, error) {
+	stmt, args, err := psql.Select(
+		sm.From("events"),
+		sm.Columns(
+			psql.Raw("max(global_position)").As("global_position"),
+		),
+	).Build()
+	if err != nil {
+		return nil, sverror.NewInternalError("error.failed.to.build.query", err)
+	}
+
+	row := es.db.QueryRow(ctx, stmt, args...)
+
+	globalPosition := new(decimal.Decimal)
+	err = row.Scan(globalPosition)
+	if err != nil {
+		return nil, sverror.NewInternalError("error.failed.to.scan.row", err)
+	}
+
+	return globalPosition, nil
 }
