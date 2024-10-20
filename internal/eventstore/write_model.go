f8423e75c6753a010704a4b9af18999bc6ff8042
driftdev <92285161+driftdev@users.noreply.github.com>
2024-10-20 21:21:16 +0530

    feat: implemented a basic write_model

---
+++ internal/eventstore/write_model.go
@@ -0,0 +1,51 @@
+package eventstore
+
+import (
+	"github.com/shopspring/decimal"
+	"time"
+)
+
+type WriteModel struct {
+	TenantId          string          `json:"-"`
+	AggregateId       string          `json:"-"`
+	AggregateSequence int64           `json:"-"`
+	Owner             string          `json:"-"`
+	Events            []Event         `json:"-"`
+	Position          decimal.Decimal `json:"-"`
+	CreatedAt         time.Time       `json:"-"`
+	UpdatedAt         time.Time       `json:"-"`
+}
+
+func (wm *WriteModel) Query() *QueryBuilder {
+	return NewQuery()
+}
+
+func (wm *WriteModel) AppendEvents(events ...Event) {
+	wm.Events = append(wm.Events, events...)
+}
+
+func (wm *WriteModel) Reduce() error {
+	if len(wm.Events) == 0 {
+		return nil
+	}
+
+	if wm.TenantId == "" {
+		wm.TenantId = wm.Events[0].GetAggregate().TenantId
+	}
+
+	if wm.AggregateId == "" {
+		wm.AggregateId = wm.Events[0].GetAggregate().Id
+	}
+
+	if wm.Owner == "" {
+		wm.Owner = wm.Events[0].GetAggregate().Owner
+	}
+
+	wm.Position = wm.Events[len(wm.Events)-1].GetPosition()
+	wm.CreatedAt = wm.Events[0].GetCreatedAt()
+	wm.UpdatedAt = wm.Events[len(wm.Events)-1].GetCreatedAt()
+
+	wm.Events = wm.Events[0:0]
+
+	return nil
+}
