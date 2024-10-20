b11d6dfeb0ce9a88bf8a4e724007ba5f002d6cf6
driftdev <92285161+driftdev@users.noreply.github.com>
2024-10-20 21:22:59 +0530

    refactor: moved transaction handling logic from eventstore to database pkg

--- internal/eventstore/eventstore.go
+++ internal/eventstore/eventstore.go
@@ -1,40 +1,9 @@
 package eventstore
 
 import (
-	"context"
-	"fmt"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/driftbase/auth/internal/database"
 )
 
 type EventStore struct {
-	tenantIds []string
-	db        pgxpool.Pool
-}
-
-func (es *EventStore) WithTx(ctx context.Context, txOptions pgx.TxOptions, fn func(tx pgx.Tx) error) error {
-	tx, err := es.db.BeginTx(ctx, txOptions)
-	if err != nil {
-		return fmt.Errorf("failed to begin transaction: %w", err)
-	}
-
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback(ctx)
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback(ctx)
-		}
-	}()
-
-	err = fn(tx)
-	if err != nil {
-		return fmt.Errorf("failed transaction execution: %w", err)
-	}
-
-	if err = tx.Commit(ctx); err != nil {
-		return fmt.Errorf("failed to commit transaction: %w", err)
-	}
-
-	return nil
+	db *database.Db
 }
