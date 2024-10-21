afd4e6b8968a29e9e0bd417963f53234a0e4cfc5
driftdev <92285161+driftdev@users.noreply.github.com>
2024-10-21 08:33:34 +0530

    feat: implemented a reduce which run in session and use txn for consistency

--- internal/eventstore/session.go
+++ internal/eventstore/session.go
@@ -118,3 +118,52 @@ func (s *Session) SaveChanges(ctx context.Context) ([]Event, error) {
 
 	return mappedEvents, nil
 }
+
+func (s *Session) Reduce(ctx context.Context, reducer Reducer) error {
+	stmt, args, err := reducer.Query().ToSQL()
+	if err != nil {
+		return err
+	}
+
+	var rows pgx.Rows
+	if s.tx != nil {
+		rows, err = s.tx.Query(ctx, stmt, args...)
+		if err != nil {
+			return err
+		}
+	} else {
+		rows, err = s.es.db.Pool.Query(ctx, stmt, args...)
+		if err != nil {
+			return err
+		}
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		event := new(EventBase)
+		if err = rows.Scan(
+			&event.Aggregate.TenantId,
+			&event.Id,
+			&event.Aggregate.Id,
+			&event.Aggregate.Type,
+			&event.Aggregate.Version,
+			&event.Aggregate.Sequence,
+			&event.Aggregate.Owner,
+			&event.Payload,
+			&event.CorrelationId,
+			&event.CausationId,
+			&event.CreatedAt,
+		); err != nil {
+			return err
+		}
+
+		mappedEvent, err := s.es.mapEvent(event)
+		if err != nil {
+			return err
+		}
+
+		reducer.AppendEvents(mappedEvent)
+	}
+
+	return reducer.Reduce()
+}
