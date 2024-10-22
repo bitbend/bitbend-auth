2f7ae54e9b7671db040786e1146e9d13f6777a4a
driftdev <92285161+driftdev@users.noreply.github.com>
2024-10-22 21:45:04 +0530

    refactor(eventstore): removed multi function spanning tx based session

--- internal/eventstore/session.go
+++ internal/eventstore/session.go
@@ -2,15 +2,12 @@ package eventstore
 
 import (
 	"context"
-	"fmt"
 	"github.com/jackc/pgx/v5"
 )
 
 type Session struct {
-	es          *EventStore
-	tx          pgx.Tx
-	startStream bool
-	commands    []Command
+	es       *EventStore
+	commands []Command
 }
 
 func (es *EventStore) NewSession(ctx context.Context) (*Session, error) {
@@ -20,21 +17,7 @@ func (es *EventStore) NewSession(ctx context.Context) (*Session, error) {
 	}, nil
 }
 
-func (es *EventStore) NewSessionTx(ctx context.Context) (*Session, error) {
-	tx, err := es.db.Pool().BeginTx(ctx, pgx.TxOptions{IsoLevel: pgx.ReadCommitted})
-	if err != nil {
-		return nil, fmt.Errorf("error starting session transaction: %v", err)
-	}
-
-	return &Session{
-		es:       es,
-		tx:       tx,
-		commands: make([]Command, 0),
-	}, nil
-}
-
 func (s *Session) StartStream(commands ...Command) *Session {
-	s.startStream = true
 	s.commands = append(s.commands, commands...)
 	return s
 }
@@ -46,69 +29,30 @@ func (s *Session) AppendEvents(commands ...Command) *Session {
 
 func (s *Session) SaveChanges(ctx context.Context) ([]Event, error) {
 	var events []Event
-
-	if s.tx != nil {
-		sequences, err := fetchLatestSequences(ctx, s.tx, s.commands)
+	err := s.es.db.WithTx(ctx, pgx.TxOptions{IsoLevel: pgx.ReadCommitted}, func(tx pgx.Tx) error {
+		sequences, err := fetchLatestSequences(ctx, tx, s.commands)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		events, err = commandsToEvents(s.commands, sequences)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		if s.startStream {
-			if err = createStreams(ctx, s.tx, events); err != nil {
-				return nil, err
-			}
+		if err = createStreams(ctx, tx, events); err != nil {
+			return err
 		}
 
-		events, err = createEvents(ctx, s.tx, events)
+		events, err = createEvents(ctx, tx, events)
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		defer func() {
-			if p := recover(); p != nil {
-				_ = s.tx.Rollback(ctx)
-				panic(p)
-			} else if err != nil {
-				_ = s.tx.Rollback(ctx)
-			}
-		}()
-
-		if err = s.tx.Commit(ctx); err != nil {
-			return nil, fmt.Errorf("failed to commit session transaction: %w", err)
-		}
-	} else {
-		err := s.es.db.WithTx(ctx, pgx.TxOptions{IsoLevel: pgx.ReadCommitted}, func(tx pgx.Tx) error {
-			sequences, err := fetchLatestSequences(ctx, tx, s.commands)
-			if err != nil {
-				return err
-			}
-
-			events, err = commandsToEvents(s.commands, sequences)
-			if err != nil {
-				return err
-			}
-
-			if s.startStream {
-				if err = createStreams(ctx, tx, events); err != nil {
-					return err
-				}
-			}
-
-			events, err = createEvents(ctx, tx, events)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		})
-		if err != nil {
-			return nil, err
-		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	mappedEvents, err := s.es.mapEvents(events)
@@ -125,17 +69,9 @@ func (s *Session) Reduce(ctx context.Context, reducer Reducer) error {
 		return err
 	}
 
-	var rows pgx.Rows
-	if s.tx != nil {
-		rows, err = s.tx.Query(ctx, stmt, args...)
-		if err != nil {
-			return err
-		}
-	} else {
-		rows, err = s.es.db.Pool().Query(ctx, stmt, args...)
-		if err != nil {
-			return err
-		}
+	rows, err := s.es.db.Pool().Query(ctx, stmt, args...)
+	if err != nil {
+		return err
 	}
 	defer rows.Close()
 
